d8b63cb5cf1528363e83321a84959b8ea18b616f
Sasse, Thomas (DI FA SEA ES&PRC CHE2) <thomas.sasse@siemens.com>
2025-03-25 19:55:09 +0000

    Add simple CMake configuration

---
+++ scripts/build_production.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get absolute path of the project directory
+SCRIPT_DIR="$(dirname $(realpath $0) )"
+PROJECT_DIR="$SCRIPT_DIR/.."
+SOURCE_DIR="$PROJECT_DIR/src"
+BUILD_DIR="$PROJECT_DIR/build"
+
+GENERATOR="Unix Makefiles"
+
+echo "=============================="
+echo "Remove existing build directory"
+echo "=============================="
+# Must delete build directory before generating new one, to avoid
+# Command line error: Option --cpu can only occur once
+if [ -d "$BUILD_DIR" ]; then
+    rm -rf "$BUILD_DIR"
+fi
+
+echo "=============================="
+echo "Generate CMake build directory"
+echo "=============================="
+
+cmake -S "$SOURCE_DIR" -B "$BUILD_DIR" -G "$GENERATOR"
+
+echo "========================"
+echo "Build Executable/Library"
+echo "========================"
+
+cmake --build "$BUILD_DIR"
